31990d674c3a05daad204447ea1a59c6e774199f
kevwong <kevwong@kevwongs-MacBook-Pro.local>
2018-10-31 16:52:13 -0700

    remove mousedown listener in detach

--- ext/language_tools.js
+++ ext/language_tools.js
@@ -1366,7 +1366,7 @@ ace.define("ace/snippets",["require","exports","module","ace/lib/oop","ace/lib/e
             this.editor.keyBinding.removeKeyboardHandler(this.keyboardHandler);
             this.editor.off("changeSelection", this.changeListener);
             this.editor.off("blur", this.blurListener);
-            this.editor.off("mousedown", this.mousedownListener);
+            //this.editor.off("mousedown", this.mousedownListener);
             this.editor.off("mousewheel", this.mousewheelListener);
             this.changeTimer.cancel();
             this.hideDocTooltip();
